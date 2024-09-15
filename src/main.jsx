a7b9e05affe165a52857671342e42271b9e72e01
nikhiludayy <nikhiluday0823@gmail.com>
2024-09-16 02:10:18 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <BrowserRouter basename="/ScreenPedia/">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>

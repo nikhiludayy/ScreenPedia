1a2a3a8757410dd6d4612843211a0067d40aab40
nikhiludayy <nikhiluday0823@gmail.com>
2024-09-16 01:19:25 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <BrowserRouter basename="ScreenPedia">
+        <HashRouter basename="ScreenPedia">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );

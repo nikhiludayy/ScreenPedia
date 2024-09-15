de07622f7149594a8cf3e3680df9ebbf58432930
nikhiludayy <nikhiluday0823@gmail.com>
2024-09-16 01:39:54 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <HashRouter>
+        <HashRouter >
             <App />
         </HashRouter>
     </Provider>
